d522be56d97b1ddb18c74df909bbd19611afe89f
Zj <519564415@qq.com>
2020-01-10 15:12:47 +0800

    :sparkles:(captcha generator)

---
+++ src/Apsk.Utils/CaptchaType.cs
@@ -0,0 +1,13 @@
+﻿// <copyright file="CaptchaType.cs" company="apsk">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace Apsk.Utils
+{
+    public enum CaptchaType
+    {
+        NumberVerifyCode,
+        AbcVerifyCode,
+        MixVerifyCode
+    }
+}
