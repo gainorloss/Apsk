88642b554fc0128d84bb1c91e629e16f4cb42c5a
Zj <519564415@qq.com>
2020-01-16 17:41:12 +0800

    :sparkles:(直送仓)

--- samples/ServiceDiscovery.ConsoleApp/Program.cs
+++ samples/ServiceDiscovery.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DnsClient;
+using Polly;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +16,14 @@ namespace ServiceDiscovery.ConsoleApp
         {
             try
             {
+                var timeOut = Policy.Timeout(2, (ctx, span, task) => Console.WriteLine($"{DateTime.Now} - 超时"));
+                var retry = Policy.Handle<Exception>()
+                    .Retry(2, (ex, i) => Console.WriteLine($"{DateTime.Now} - 重试第{i}次:{ex.Message}"));
+                var breaker = Policy.Handle<Exception>()
+                    .CircuitBreaker(2, TimeSpan.FromSeconds(2),(ex, span) => Console.WriteLine($"{DateTime.Now} - 中断:{span.TotalSeconds}s"),()=> Console.WriteLine($"{DateTime.Now} - 中断:恢复"));
+
+                var fallback = Policy.Handle<Exception>().Fallback(()=> Console.WriteLine($"{DateTime.Now} - 降级"));
+
                 var services = new[] { "CatalogItemsAPI","UsersAPI" };
                 var dnsQuery = new LookupClient(IPAddress.Parse("127.0.0.1"),8600);
 
