36fc84516ed1da879fb9d5b4c5124dfe7eb804c0
Zj <519564415@qq.com>
2020-01-16 18:11:12 +0800

    :sparkles:(polly)

--- samples/ServiceDiscovery.ConsoleApp/Program.cs
+++ samples/ServiceDiscovery.ConsoleApp/Program.cs
@@ -20,35 +20,37 @@ namespace ServiceDiscovery.ConsoleApp
                 var retry = Policy.Handle<Exception>()
                     .Retry(2, (ex, i) => Console.WriteLine($"{DateTime.Now} - 重试第{i}次:{ex.Message}"));
                 var breaker = Policy.Handle<Exception>()
-                    .CircuitBreaker(2, TimeSpan.FromSeconds(2),(ex, span) => Console.WriteLine($"{DateTime.Now} - 中断:{span.TotalSeconds}s"),()=> Console.WriteLine($"{DateTime.Now} - 中断:恢复"));
+                    .CircuitBreaker(2, TimeSpan.FromSeconds(2), (ex, span) => Console.WriteLine($"{DateTime.Now} - 中断:{span.TotalSeconds}s"), () => Console.WriteLine($"{DateTime.Now} - 中断:恢复"));
 
-                var fallback = Policy.Handle<Exception>().Fallback(()=> Console.WriteLine($"{DateTime.Now} - 降级"));
+                var fallback = Policy.Handle<Exception>().Fallback(() => Console.WriteLine($"{DateTime.Now} - 降级"));
 
-                var services = new[] { "CatalogItemsAPI","UsersAPI" };
-                var dnsQuery = new LookupClient(IPAddress.Parse("127.0.0.1"),8600);
+
+                var services = new[] { "CatalogItemsAPI", "UsersAPI" };
+                var dnsQuery = new LookupClient(IPAddress.Parse("127.0.0.1"), 8600);
 
                 while (true)
                 {
-                    Thread.Sleep(3000);
-                    foreach (var service in services)
-                    {
-                        var entries = await dnsQuery.ResolveServiceAsync("service.consul", service);
-                        var entry = entries.FirstOrDefault();
-                        if (entry != null)
+                    Policy.Wrap(fallback, breaker, retry, timeOut)
+                        .Execute(() =>
                         {
-                            if (!keyValuePairs.TryGetValue(entry.HostName,out var count))
+                            for (int i = 0; i < 10; i++)
                             {
-                                keyValuePairs.Add(entry.HostName,1);
+                                try
+                                {
+                                    var entries = dnsQuery.ResolveServiceAsync("service.consul", "UsersAPI").Result;
+                                    var entry = entries.FirstOrDefault();
+                                    if (entry != null)
+                                    {
+                                        Console.WriteLine($"{DateTime.Now} - 请求:{entry.AddressList.Any()}成功");
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    throw;
+                                }
+                                Task.Delay(1000).Wait();
                             }
-                            else
-                            {
-                                keyValuePairs[entry.HostName] = count + 1;
-                            }
-                        }
-
-                        Console.WriteLine(string.Join(",",keyValuePairs.Select(pair=>$"{pair.Key}:{pair.Value}")));
-                    }
-
+                        });
                 }
             }
             catch (Exception ex)
